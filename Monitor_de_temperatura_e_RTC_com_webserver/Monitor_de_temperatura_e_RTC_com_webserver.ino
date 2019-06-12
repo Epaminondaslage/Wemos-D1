7865ee16e74c9766fb61aa894af62a56c5bc39d2
EpaminondasLage <epaminondaslage@gmail.com>
2019-06-11 21:52:35 -0300

    Update Monitor_de_temperatura_e_RTC_com_webserver.ino
    Melhoramentos diversos, fixa IP, armazena temp max e min

--- Monitor_de_temperatura_e_RTC_com_webserver/Monitor_de_temperatura_e_RTC_com_webserver.ino
+++ Monitor_de_temperatura_e_RTC_com_webserver/Monitor_de_temperatura_e_RTC_com_webserver.ino
@@ -1,3 +1,10 @@
+/******************************************************************
+        Monitoramento de temperatura com HDC1080 e
+       Dados mostrados na porta serial e    servidor web
+    Servidor web com IP fixo e cliente de rede wifi existente
+    Elaborado por Epaminondas Lage - Junho de 2019
+ ******************************************************************/
+
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -5,8 +12,7 @@
 #include <Wire.h>
 #include "ClosedCube_HDC1080.h"
 #include "RTClib.h"
-//#include <LiquidCrystal_I2C.h>
-//teste em sala
+
 // inicializa RTC
 // Usar lib ->https://github.com/adafruit/RTClib
 RTC_DS3231 rtc;
@@ -14,40 +20,25 @@ RTC_DS3231 rtc;
 //Inicializa Sensor de temperatura e umidade HDC 1080
 ClosedCube_HDC1080 hdc1080;
 
-// declara variáveis globais
+// declara variáveis globais para dias da semana e meses do ano
 char daysOfTheWeek[7][14] = {"Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sabado"};
+char mesesdoano [12][10] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio" , "Junho", "Julho" , "Agosto", "Setembro", "Outubro" , "Novembro" , "Dezembro"};
 
-//Array que desenha o simbolo de grau no display
-byte grau[8] = {B00110, B01001, B01001, B00110,
-                B00000, B00000, B00000, B00000,
-               };
 
-//Inicializa o display no endereco 0x3B
-//Define o numero de colunas e linhas do display
-//LiquidCrystal_I2C lcd(0x3B, 16, 2);
-
-// Rede wifi 1
-//const char* ssid = "pe de serra extra";
-//const char* password = "planetfone";
-
-// rede wifi 2
-//const char* ssid = "123644987";
-//const char* password = "planeta514";
-
-// rede wifi 3
-const char* ssid = "Popo";
-const char* password = "Planetfone";
+// Configuração para acesso à rede wifi
+const char* ssid = "pe de serra extra";
+const char* password = "planetfone";
 
 // Declara variáveis de armazenamento minimo e maximo
 float tmaxima;
 float tminima;
-int diamin, mesmin, anomin, horamin, minutomin, segundomax,diamax, mesmax, anomax, horamax, minutomax, segundomin;
-String diadasemanamin,diadasemanamax;
+int diamin, mesmin, anomin, horamin, minutomin, segundomax, diamax, mesmax, anomax, horamax, minutomax, segundomin;
+String diadasemanamin, diadasemanamax;
 
 int a = 0;
 
 
-
+// inicializa servidor web
 ESP8266WebServer server(80);
 
 void handleRoot() {
@@ -75,7 +66,7 @@ void setup(void) {
 
   //inicializa parâmetros do RTC
   Wire.begin(4, 5);
-  
+
 #ifndef ESP8266
   while (!Serial);
 #endif
@@ -86,7 +77,7 @@ void setup(void) {
     Serial.println("RTC não encontrado");
     while (1);
   }
-  
+
   //Rotina para setar as horas do RTC quando perde alimentação
   if (rtc.lostPower()) {
     Serial.println("RTC pedeu alimentação, favor acertar as horas!");
@@ -105,21 +96,6 @@ void setup(void) {
   //Inicializa o sensor de temperatura HDC1080 no endereço 0x40
   hdc1080.begin(0x40);
 
-  /*//Inicializa o display LCD I2C
-    lcd.init();
-    lcd.backlight();
-
-    //Atribui a "1" o valor do array "grau", que desenha o simbolo de grau
-    lcd.createChar(1, grau);
-
-    //Mostra no display as informações iniciais
-    lcd.setCursor(0, 0);
-    lcd.print("Temp.: XX.XX  C");
-    lcd.setCursor(0, 1);
-    lcd.print("Umid.: YY.YY %");
-
-  */
-
   // Inicializa conexão wifi
   pinMode(BUILTIN_LED, OUTPUT);
   digitalWrite(BUILTIN_LED, 0);
@@ -127,6 +103,10 @@ void setup(void) {
   delay(1000);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
+
+  // configura WEMOS com IP fixo
+  IPAddress subnet(255, 255, 255, 0);
+  WiFi.config(IPAddress(10, 0, 0, 23), IPAddress(10, 0, 0, 1), subnet);
   Serial.println("");
 
   // Espera por conexão wifi pisca led na placa
@@ -167,12 +147,13 @@ void setup(void) {
 }
 
 void loop(void) {
+
   //Mostra dados do RTC na porta serial
   DateTime now = rtc.now();
   Serial.print("Data :");
   Serial.print(now.day(), DEC);
   Serial.print('/');
-  Serial.print(now.month(), DEC);
+  Serial.print(mesesdoano[now.month() - 1]);
   Serial.print('/');
   Serial.print(now.year(), DEC);
   Serial.print(" (");
@@ -188,15 +169,7 @@ void loop(void) {
   delay(2000);
   float t = hdc1080.readTemperature();
   float h = hdc1080.readHumidity();
- 
-  /*Mostra os valores no display
-    lcd.setCursor(7, 0);
-    lcd.print(t);
-    lcd.setCursor(13, 0);
-    lcd.write(1);
-    lcd.setCursor(7, 1);
-    lcd.print(h);*/
- 
+
   //Mostra os valores no Serial Monitor
   Serial.print("Temperatura: ");
   Serial.print(t);
@@ -207,11 +180,11 @@ void loop(void) {
   Serial.println();
   digitalWrite(BUILTIN_LED, 0);
   delay(2000);
- 
+
   // Calcula a temp max e min
   if (hdc1080.readTemperature() > tmaxima) {
     tmaxima = hdc1080.readTemperature();
-        DateTime now   = rtc.now();
+    DateTime now   = rtc.now();
     diamax         = now.day(), DEC;
     mesmax         = now.month(), DEC;
     anomax         = now.year(), DEC;
@@ -244,20 +217,21 @@ String homePage() {
   String tempmin     = String(tminima);
   String temp        = String(hdc1080.readTemperature());
   String umid        = String(hdc1080.readHumidity());
-  String dmin        = diamin<10 ? "0" + String(diamin) : String(diamin);
-  String mmin        = mesmin<10 ? "0" + String(mesmin) : String(mesmin);
+  String dmin        = diamin < 10 ? "0" + String(diamin) : String(diamin);
+  String mmin        = mesmin < 10 ? "0" + String(mesmin) : String(mesmin);
   String amin        = String(anomin);
-  String hmin        = horamin<10 ? "0" + String(horamin) : String(horamin);
-  String mimin       = minutomin<10 ? "0" + String(minutomin) : String(minutomin);
-  String smin        = segundomin<10 ? "0" + String(segundomin) : String(segundomin);
-  String dmax        = diamax<10 ? "0" + String(diamax) : String(diamax);
-  String mmax        = mesmax<10 ? "0" + String(mesmax) : String(mesmax);
+  String hmin        = horamin < 10 ? "0" + String(horamin) : String(horamin);
+  String mimin       = minutomin < 10 ? "0" + String(minutomin) : String(minutomin);
+  String smin        = segundomin < 10 ? "0" + String(segundomin) : String(segundomin);
+  String dmax        = diamax < 10 ? "0" + String(diamax) : String(diamax);
+  String mmax        = mesmax < 10 ? "0" + String(mesmax) : String(mesmax);
   String amax        = String(anomax);
-  String hmax        = horamax<10 ? "0" + String(horamax) : String(horamax);
-  String mimax       = minutomax<10 ? "0" + String(minutomax) : String(minutomax);
-  String smax        = segundomax<10 ? "0" + String(segundomax) : String(segundomax);
+  String hmax        = horamax < 10 ? "0" + String(horamax) : String(horamax);
+  String mimax       = minutomax < 10 ? "0" + String(minutomax) : String(minutomax);
+  String smax        = segundomax < 10 ? "0" + String(segundomax) : String(segundomax);
   DateTime now       = rtc.now();
   String diadasemana = String(daysOfTheWeek[now.dayOfTheWeek()]);
+  //String dia         = (dia.toInt() < 10 ? "0" + dia : dia);
   String dia         = String(now.day(), DEC);
   String mes         = String(now.month(), DEC);
   String ano         = String(now.year(), DEC);
@@ -273,19 +247,13 @@ String homePage() {
            "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
            "<span style='display: none;'>" + String(0) + "</span>"
            "<div style='text-align: center;'><img src =  'http://caixagg.planetfone.com.br/img/Sitiopedeserra.png' alt='' width='290' height=63' /></div>"
-           "<div style='text-align: center;'>" + diadasemana + ", " + dia + "/" + mes + "/" + ano + "</div>"
+           "<div style='text-align: center;'>" + diadasemana + ", " + dia + " de " + mesesdoano[now.month() - 1] + " de " + ano + "</div>"
            "<h1>Temperatura: " + temp + "<sup>o</sup>C</h1>"
            "<h1>Umidade: " + umid + "%</h1>"
-           "<table><tbody><tr><td><img src='http://caixagg.planetfone.com.br/img/Tmin.png' alt=' ' width='20' height='35' /></td><td><h4>Temperatura Mínima: " + tempmin + " <sup>o</sup>C</h4>" + diadasemanamin + ", " + dmin + "/ " + mmin + "/ " + amin + " [" + hmin + ":" + mimin +":" + smin +  "] </td></tr></tbody></table>"
-           "<table><tbody><tr><td><img src='http://caixagg.planetfone.com.br/img/Tmax.png' alt=' ' width='20' height='35' /></td><td><h4>Temperatura Máxima: " + tempmax + " <sup>o</sup>C</h4>" + diadasemanamax + ", " + dmax + "/ " + mmax + "/ " + amax + " [" + hmax + ":" + mimax +":" + smax +  "] </td></tr></tbody></table>"
-
-      );
- /*
-           //  comandos sem a tabela apenas para guardar depois eu mato
-           "<p><img src =  'http://caixagg.planetfone.com.br/img/Tmin.png' alt='' width='20' height=35'><h3>Temperatura Mínima: " + tempmin + "<sup>o</sup>C</h3></p>"
-           "<div style='text-align: left;'>" + diadasemanamin + ", " + dmin + "/" + mmin + "/" + amin + " [" + hmin + ":" + mimin + ":" + smin + "] "+ "</div>"
-           "<p><img src =  'http://caixagg.planetfone.com.br/img/Tmax.png' alt='' width='20' height=35'><h3>Temperatura Máxima: " + tempmax + "<sup>o</sup>C</h3>"
-           "<div style='text-align: left;'>" + diadasemanamax + ", " + dmax + "/" + mmax + "/" + amax + " [" + hmax + ":" + mimax + ":" + smax + "] "+ "</div>"
+           "<table><tbody><tr><td><img src='http://caixagg.planetfone.com.br/img/Tmin.png' alt=' ' width='20' height='35' /></td><td><h4><span style='color: #87CEFA;'>Temperatura Mínima: " + tempmin + " <sup>o</sup>C</span></h4>" + diadasemanamin + ", " + dmin + "/ " + mmin + "/ " + amin + " [" + hmin + ":" + mimin + ":" + smin +  "] </td></tr></tbody></table>"
+           "<table><tbody><tr><td><img src='http://caixagg.planetfone.com.br/img/Tmax.png' alt=' ' width='20' height='35' /></td><td><h4><span style='color: #ff0000;'>Temperatura Máxima: " + tempmax + " <sup>o</sup>C</span></h4>" + diadasemanamax + ", " + dmax + "/ " + mmax + "/ " + amax + " [" + hmax + ":" + mimax + ":" + smax +  "] </td></tr></tbody></table>"
            "<p>&nbsp;</p>"
- */          
+           "<p>&nbsp;</p>"
+           "<div style='text-align: center;'>&copy 2019 - Epaminondas Lage </div>"
+         );
 }
